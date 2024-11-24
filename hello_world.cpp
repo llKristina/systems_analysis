2085d0b9b3921bbdc1ec0095ae2cbdaa4330befb
llKristina <kristina20052020@gmail.com>
2024-11-24 18:59:36 +0300

    реализация программы hello world

---
+++ hello_world.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string name;
+    cout << "Введите имя: ";
+    cin >> name;
+    cout << "Hello world from " << name << endl;
+    return 0;
+}
