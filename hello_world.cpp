c7365cbb3ff21031aaddace46662d5200f400a78
llKristina <kristina20052020@gmail.com>
2024-11-24 19:03:30 +0300

    Hello world с именем пользователя

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 using namespace std;
-int main(){
-  cout << "Hello world!" << endl;
-  return 0;
+
+int main() {
+    string name;
+    cout << "Введите имя: ";
+    cin >> name;
+    cout << "Hello world from " << name << endl;
+    return 0;
 }
